40f90977234a8d96a196a5cf6ed8f9d55386c819
vacuum113 <dimentr12170@gmail.com>
2019-06-12 14:20:01 +0500

    again1

--- features/step-definitions/then.js
+++ features/step-definitions/then.js
@@ -4,9 +4,11 @@ const { Then } = require('cucumber');
 
 Then("User expects to see a page with the title {string}", function(string1) {
     let title;
+
     browser.waitUntil(function() {
         return (title = browser.getTitle()) == string1;
     }, 5000);
+
     assert.equal(title, string1);
 });
 
@@ -15,10 +17,11 @@ Then("User expects to receive API version {string} documentation on the site wit
 });
 
 Then('User expect to see a notification that this is the old version of the API', function() {
-    let notification;
-    if (!(notification = $('body > div:nth-child(9)'))) {
-        notification = $('body> div:nth-child(13)');
-    }
+    let notification = $('body > div:nth-child(9)');
+
+    if (!(notification.$('a').getText() === 'click here.'))
+        notification = $('body> div:nth-child(7)');
+
     assert.equal(notification.$('a').getText(), 'click here.');
 });
 
@@ -26,8 +29,10 @@ Then('Also user expect to see link to a newer version of API documentation', fun
     let notificationLink = $('body > div:nth-child(9)');
     notificationLink.$('a').click();
     browser.pause(1000);
+
     browser.switchTab(browser.getTabIds()[1]);
     let title = browser.getTitle();
     browser.close();
+
     assert.equal(title, 'Introduction — Vue.js');
 });
--- features/step-definitions/when.js
+++ features/step-definitions/when.js
@@ -4,17 +4,21 @@ const { When } = require('cucumber');
 
 When("User selects the Learn link in the navigation bar", function() {
     let linkLearn;
+
     browser.waitUntil(function() {
         return (linkLearn = $$('li.nav-dropdown-container')[0].$('a')).getText() === "Learn";
     }, 5000);
+
     linkLearn.moveToObject();
 });
 
 When("User clicks on the API link in the drop-down menu", function() {
     let linkAPI;
+
     browser.waitUntil(function() {
         return (linkAPI = $$('li.nav-dropdown-container')[0].$('ul').$$('a.nav-link')[1]).getText() === "API";
     }, 5000);
+
     linkAPI.moveToObject();
     linkAPI.click();
 });
@@ -22,6 +26,7 @@ When("User clicks on the API link in the drop-down menu", function() {
 
 When("User select the option  {int} with the value {string} for element select: {string}", function(int1, string1, string2) {
     let versionSelect;
+
     browser.waitUntil(function() {
         return (versionSelect = $(string2));
     }, 5000);
